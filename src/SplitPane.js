c5501289bb589f3534af7f4ed905c1d6f00c0629
JackHou <vjack070707@gmail.com>
2016-03-04 00:19:35 +0800

    fxi panel 2 size

--- src/SplitPane.js
+++ src/SplitPane.js
@@ -81,6 +81,8 @@ export default React.createClass({
                         const pane2Size = size + (position - current);
                         if (pane2Size >= this.props.minSize && newSize >= this.props.minSize) {
                             this.updateNewSize(newSize);
+                        } else if (pane2Size < this.props.minSize) {
+                            this.refs.panel2.setState({size: this.props.minSize})
                         }
                     } else if (newSize >= this.props.minSize) {
                         this.updateNewSize(newSize);
